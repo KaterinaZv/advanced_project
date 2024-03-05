c0178ebb9b2aea0ad81560adcdfdbeac6fd22af0
KaterinaZv <katuha.hip.hop@gmail.com>
2024-03-05 17:58:09 +0300

    add redux-toolkit

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'operator-linebreak': 'off',
+    'no-param-reassign': 'off',
   },
   globals: {
     __IS_DEV__: true,
