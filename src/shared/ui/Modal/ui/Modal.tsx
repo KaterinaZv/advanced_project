d1189313f40fc4ba04e847ae9ba06deafa86596d
KaterinaZv <katuha.hip.hop@gmail.com>
2024-03-06 14:16:29 +0300

    fix theme

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -7,7 +7,6 @@ import React, {
 } from 'react'
 import { Portal } from 'shared/ui/Portal'
 import { classNames } from 'shared/lib/classNames/classNames'
-import { useTheme } from 'app/providers/ThemeProvider'
 import cls from './Modal.module.scss'
 
 interface ModalProps {
@@ -22,8 +21,6 @@ const ANIMATION_DELAY = 1000
 export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
   const [isClosing, setIsClosing] = useState<boolean>(false)
   const timerRef = useRef<ReturnType<typeof setTimeout>>()
-
-  const { theme } = useTheme()
 
   const closeHandler = useCallback(() => {
     if (onClose) {
@@ -65,7 +62,7 @@ export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
 
   return (
     <Portal>
-      <div className={classNames(cls.Modal, mods, [className, theme])}>
+      <div className={classNames(cls.Modal, mods, [className])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div className={cls.content} onClick={onContentClick}>
             {children}
