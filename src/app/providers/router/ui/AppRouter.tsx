7384f46f4bd6be6196028877fef516d4d4aa37cb
KaterinaZv <katuha.hip.hop@gmail.com>
2024-04-01 15:01:20 +0300

    update app router

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,33 +1,30 @@
-import { getUserAuthData } from 'entities/User'
-import { Suspense, memo, useMemo } from 'react'
-import { useSelector } from 'react-redux'
+import { Suspense, memo, useCallback } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import {
+  AppRoutesProps,
+  routeConfig,
+} from 'shared/config/routeConfig/routeConfig'
 import { PageLoader } from 'widgets/PageLoader'
+import { RequireAuth } from './RequireAuth'
 
 export const AppRouter = memo(() => {
-  const isAuth = useSelector(getUserAuthData)
+  const renderWithWrapper = useCallback((route: AppRoutesProps) => {
+    const element = (
+      <Suspense fallback={<PageLoader />}>
+        <div className='page-wrapper'>{route.element}</div>
+      </Suspense>
+    )
 
-  const routes = useMemo(() => {
-    return Object.values(routeConfig).filter((route) => {
-      if (route.authOnly && !isAuth) return false
-      return true
-    })
-  }, [isAuth])
+    return (
+      <Route
+        key={route.path}
+        path={route.path}
+        element={
+          route.authOnly ? <RequireAuth>{element}</RequireAuth> : element
+        }
+      />
+    )
+  }, [])
 
-  return (
-    <Routes>
-      {routes.map((value) => (
-        <Route
-          key={value.path}
-          path={value.path}
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <div className='page-wrapper'>{value.element}</div>
-            </Suspense>
-          }
-        />
-      ))}
-    </Routes>
-  )
+  return <Routes>{Object.values(routeConfig).map(renderWithWrapper)}</Routes>
 })
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,5 +1,13 @@
 import { getUserAuthData } from './model/selector/getUserAuthData/getUserAuthData'
+import { getUserInited } from './model/selector/getUserInited/getUserInited'
 import { userActions, userReducer } from './model/slice/UserSlice'
 import { User, UserSchema } from './model/types/user'
 
-export { userReducer, userActions, User, UserSchema, getUserAuthData }
+export {
+  userReducer,
+  userActions,
+  User,
+  UserSchema,
+  getUserAuthData,
+  getUserInited,
+}
